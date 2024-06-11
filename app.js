b069da8c840c2fe0039c5a6f64880a58a960faf6
Maria Iurchenko <pomazkinam1997@gmail.com>
2024-06-11 01:04:02 -0600

    fix

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const os = require("os");
+const os = require('os');
+const MongoClient = require('mongodb').MongoClient;
+
 const userInfo = os.userInfo();
 const uid = userInfo.uid;
 const name = userInfo.username;
@@ -7,41 +9,39 @@ const name = userInfo.username;
 const hostname = '0.0.0.0';
 const port = 3030;
 
-const MongoClient = require("mongodb").MongoClient;
-
-const url = "mongodb://127.0.0.1:27017/";
+const url = 'mongodb://127.0.0.1:27017/';
 const mongoClient = new MongoClient(url);
-count = 0;
+let count = 0;
 
 async function run() {
     try {
         await mongoClient.connect();
-        const db = mongoClient.db(`test1`);
+        const db = mongoClient.db('test1');
         const result = await db.command({ ping: 1 });
-        console.log("Подключение с сервером успешно установлено");
-        const collection = db.collection("users");
+        console.log('Подключение с сервером успешно установлено');
+
+        const collection = db.collection('users');
         count = await collection.countDocuments();
         console.log(`В коллекции users ${count} документа/ов`);
         console.log(result);
-    }catch(err) {
-        console.log("Возникла ошибка");
+    } catch (err) {
+        console.log('Возникла ошибка');
         console.log(err);
     } finally {
         await mongoClient.close();
-        console.log("Подключение закрыто");
+        console.log('Подключение закрыто');
     }
 }
- run().catch(console.error);
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello ${name}, you have ${count} documents`);
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Запускаем асинхронную функцию и затем HTTP-сервер
+run().then(() => {
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Hello ${name}, you have ${count} documents`);
+    });
 
-const collection = db.collection("users");
-const count = await collection.countDocuments();
-console.log(`В коллекции users ${count} документа/ов`);
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+}).catch(console.error);
